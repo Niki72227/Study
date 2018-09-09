31af6e7d074c1813179b7bdbde3881df1cfa03a9
Niki Chirkinyan <nikolay72227@gmail.com>
2018-09-09 19:30:33 +0300

    1. Создать словарь со студентами и оценками поиграться с ним(поменять оценки добавить/ исключить студентов посчитать сумму балов и среднее арифметическое) 2. Создать словарь месяцы - дни вывести месяцы и дни двумя способами он там сказал как он хочет я так и сделал 3. Шахматная доска проверить поле белое или черное

--- MyPlayground.playground/Contents.swift
+++ MyPlayground.playground/Contents.swift
@@ -28,7 +28,7 @@ if Int(b) < 20{
 
 
 //Базовые операторы
-
+/*
 enum Month : Int {
     case January, February, March, April, May, June, July, August, September, October, November, December
 }
@@ -57,8 +57,10 @@ case .October, .November, .December:
 }
 
 print("C начала года до моего др прошло \(day*counter) секунд \nЯ родлися в \(quartal)-ом квартале")
+*/
 
-
+//Arrays
+/*
 var dayMonth = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
 var nameMonth = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"]
 
@@ -67,6 +69,69 @@ while i > 0 {
     i -= 1
     print("\(dayMonth[i])" + " " + nameMonth[i])
 }
+*/
+
+//Dictionaries
+
+
+var dict = ["John Black" : 10, "Katrin Snow" : 8, "Eddy Maxwell" : 6, "Ned Ford" : 12, "Barry White" : 4]
+
+dict.updateValue(9, forKey: "Katrin Snow")
+dict["Eddy Maxwell"] = 9
+
+dict["Jinny Doley"] = 10
+dict["Max Tron"] = 3
+
+dict.removeValue(forKey: "Barry White")
+
+dict
+
+var sum = 0
+for i in Array(dict.values){
+    sum += i
+    
+}
+print(sum)
+print(sum/dict.values.count)
+
+print("\n")
+
+var monthDict = ["January": 31, "February": 28 , "March": 31, "April": 30, "May": 31, "June": 30, "July": 31, "August": 31, "September": 30, "October": 31, "November": 30, "December": 31]
+for (key, value) in monthDict{
+    print("\(key)" + " " + "\(value)")
+}
+
+print("\n")
+
+for key in Array(monthDict.keys){
+    print("\(key)" + " " + "\(monthDict[key]!)")
+}
+
+print("\n")
+
+var chessLetters = ["a", "b", "c", "d", "e", "f", "g", "h"]
+var chessNumbers = [1, 2, 3, 4, 5, 6, 7, 8]
+var chessDict = [String:Bool]()
+
+for (index, letter) in chessLetters.enumerated() {
+    
+    for (index2, number) in chessNumbers.enumerated() {
+        
+        if (index % 2 == 0 && index2 % 2 == 0) || (index % 2 != 0 && index2 % 2 != 0) {
+            chessDict = [letter + "\(number)" : false]
+        } else {
+            chessDict = [letter + "\(number)" : true]
+        }
+        
+        for (key, value) in  chessDict {
+            print(key + " - " + "\(value)")
+        }
+    }
+}
+
+
+
+// Switch
 
 
 
